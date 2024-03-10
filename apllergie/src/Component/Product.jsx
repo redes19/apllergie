415d39938ecd997780aa79f0a63ad2b7db571d22
Loris Laurenti <lloris19@outlook.fr>
2024-03-10 18:56:54 +0100

    add searchbarre

--- apllergie/src/Component/Product.jsx
+++ apllergie/src/Component/Product.jsx
@@ -1,36 +1,50 @@
-import {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+export default function DisplayProduct() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState('');
 
-export default function Product() {
-    const [products, setProducts] = useState([]);
-  
-    useEffect(() => {
-      const fetchProducts = async () => {
-        try {
-          const response = await axios.get('https://world.openfoodfacts.org/cgi/search.pl?search_terms=&page_size=21&json=true');
-          if (!response.data || !response.data.products) {
-            throw new Error('No products found');
-          }
-          setProducts(response.data.products);
-        } catch (error) {
-          console.error('Fetch error:', error);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get(`https://world.openfoodfacts.org/cgi/search.pl?search_terms=${searchTerm}&page_size=21&json=true`);
+        if (!response.data || !response.data.products) {
+          throw new Error('No products found');
         }
-      };
-  
-      fetchProducts();
-    }, []);
+        setProducts(response.data.products);
+        setLoading(false); 
+      } catch (error) {
+        console.error('Fetch error:', error);
+        setLoading(false); 
+      }
+    };
+
+    fetchProducts();
+  }, [searchTerm]);
+
+  const handleSearchChanger = (event) => {
+    setSearchTerm(event.target.value);
+  };
 
   return (
     <div>
-        <h1>Liste des Produits</h1>
+        <input
+        type="texte"
+        placeholder="Rechercher un produit"
+        value={searchTerm}
+        onChange={handleSearchChanger}
+        ></input>
+      <h1>Liste des Produits</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
         <ul>
           {products.map((product, index) => (
             <li key={index}>
               <h2>{product.product_name_fr}</h2>
               <img src={product.image_url} alt={product.product_name} />
-              <p>Marque: {product.brands}</p>
-              <p>Catégories: {product.categories}</p>
               <p>Grade nutritionnel: {product.nutrition_grade_fr}</p>
               <p>Ingrédients: {product.ingredients_text}</p>
               <p>Informations nutritionnelles:</p>
@@ -38,6 +52,7 @@ export default function Product() {
             </li>
           ))}
         </ul>
+      )}
     </div>
-  )
+  );
 }

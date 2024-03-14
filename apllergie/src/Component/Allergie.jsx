d85266b6bf05785830e344624404254219146d6f
Loris Laurenti <lloris19@outlook.fr>
2024-03-14 22:57:20 +0100

    add search barre allergenes

--- apllergie/src/Component/Allergie.jsx
+++ apllergie/src/Component/Allergie.jsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 export default function Allergie() {
     const [allergenes, setAllergenes] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [searchTerm, setSearchTerm] = useState('');
 
     useEffect(() => {
         const fetchAllergenes = async () => {
@@ -12,8 +13,9 @@ export default function Allergie() {
                 console.log("allergens", response.data.tags, "data", response.data);
                 if (response.data && response.data.tags) {
                     const filteredAllergenes = response.data.tags.filter(allergene => {
-                        const language = ['en:', 'fr:']; 
-                        return language.some(langCode => allergene.id.startsWith(langCode));
+                        const language = ['en:']; 
+                        return language.some(langCode => allergene.id.startsWith(langCode)) && 
+                            allergene.name.toLowerCase().includes(searchTerm.toLowerCase());
                     });
                     setAllergenes(filteredAllergenes);
                     setLoading(false);
@@ -24,20 +26,30 @@ export default function Allergie() {
             }
         };
         fetchAllergenes();
-    }, []);
+    }, [searchTerm]);
+
+    const handleSearchChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
 
     return (
         <div>
+            <input
+                type="text"
+                placeholder="Rechercher un allergène"
+                value={searchTerm}
+                onChange={handleSearchChange}
+            />
             <h1>Liste des Allergènes</h1>
             {loading ? (
                 <p>loading...</p>
             ) : (
-            <ul>
-                {allergenes.map((allergene, index) => (
-                <li key={index}>{allergene.name}</li>
-            ))}
-          </ul>
+                <ul>
+                    {allergenes.map((allergene, index) => (
+                        <li key={index}>{allergene.name}</li>
+                    ))}
+                </ul>
             )}
         </div>
-      );
-} 
+    );
+}
